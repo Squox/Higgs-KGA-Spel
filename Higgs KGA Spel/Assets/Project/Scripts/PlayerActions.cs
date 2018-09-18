1b4c1797b53f7e9d2fb335f0bcf490bebcb31542
Squox <tassilo.radon@gmail.com>
2018-09-18 14:25:43 +0200

    Added input script and player object

---
+++ Higgs KGA Spel/Assets/Project/Scripts/PlayerActions.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerActions : MonoBehaviour {
+
+	void Start () {
+		
+	}
+	
+	void FixedUpdate () {
+        if (Input.GetKey(KeyCode.A))
+        {
+
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+
+        }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+
+        }
+        if (Input.GetKey(KeyCode.Space))
+        {
+
+        }
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+
+        }
+        if (Input.GetKey(KeyCode.Tab))
+        {
+
+        }
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+
+        }
+    }
+}
