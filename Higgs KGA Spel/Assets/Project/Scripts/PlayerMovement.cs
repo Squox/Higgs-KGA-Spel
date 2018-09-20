e187bc75021ad520aebb4fdc48e8c3242c7e2d59
AlexEnchanter <xalexo112@gmail.com>
2018-09-20 10:27:48 +0200

    Added Input and player movement

---
+++ Higgs KGA Spel/Assets/Project/Scripts/PlayerMovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private GameObject Player;
+    private Rigidbody2D PlayerRB;
+
+    private PlayerInput PlayerInputScript;
+
+    [SerializeField]private float speed;
+
+	// Use this for initialization
+	void Start ()
+    {
+        PlayerRB = FindObjectOfType<Rigidbody2D>();
+
+        Player = GameObject.FindGameObjectWithTag("Player");
+
+        PlayerInputScript = Player.GetComponent<PlayerInput>();
+	}
+
+    private void Update()
+    {
+        Debug.Log(PlayerRB.velocity.x);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate ()
+    {
+        PlayerRB.velocity = new Vector2(PlayerInputScript.MoveDirection * speed * Time.fixedDeltaTime, PlayerRB.velocity.y);
+	}
+}
