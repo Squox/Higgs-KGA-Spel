0aeda51c3f15d9ab9320641d1379357dda78085f
AlexEnchanter <xalexo112@gmail.com>
2018-09-20 08:53:42 +0200

    Hello

---
+++ Higgs KGA Spel/Assets/Project/Scripts/PlayerInput.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    // Ints:
+    public int MoveDirection = 0;
+
+
+	// Use this for initialization
+	private void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	private void Update ()
+    {
+        CheckPlayerInput();	
+	}
+
+   
+
+    private void CheckPlayerInput()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            MoveDirection = -1;
+        }
+        else if (Input.GetKeyUp(KeyCode.A))
+        {
+            MoveDirection = 0;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            MoveDirection = 1;
+        }
+        else if (Input.GetKeyUp(KeyCode.D))
+        {
+            MoveDirection = 0;
+        }
+    }
+}
