cfd0afb30e4048876c387d89ce5717e5fca36be9
AlexEnchanter <xalexo112@gmail.com>
2018-09-23 20:01:45 +0200

    Added player movement and corresponding inputs
    Added player movement on the x axis, jumping and a flip mechanism, to flip the player in the right direction.

--- Higgs KGA Spel/Assets/Project/Scripts/PlayerMovement.cs
+++ Higgs KGA Spel/Assets/Project/Scripts/PlayerMovement.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    //To store PlayerInput script and player in a local variable
     private GameObject Player;
-    private Rigidbody2D PlayerRB;
-
     private PlayerInput PlayerInputScript;
 
-    [SerializeField]private float speed;
 
-	// Use this for initialization
-	void Start ()
+    private Rigidbody2D PlayerRB;
+
+    //Floats:
+    [SerializeField] private float speed;
+    [SerializeField] private float JumpForce;
+
+    //Ints:
+    private int JumpsLeft;
+    [SerializeField] private int Jumps;
+
+    //Bools:
+    private bool IsFacingRight = true;
+
+    //variables used to check if player is on ground
+    private bool IsOnGround;
+    [SerializeField] private float CheckRadius;
+    [SerializeField] private LayerMask WhatIsGround;
+    [SerializeField] private Transform GroundCheck;
+
+    private void Start ()
     {
         PlayerRB = FindObjectOfType<Rigidbody2D>();
 
@@ -23,12 +39,62 @@ public class PlayerMovement : MonoBehaviour
 
     private void Update()
     {
-        Debug.Log(PlayerRB.velocity.x);
+        CheckFacingDirection();
+        if (IsOnGround)
+        {
+            JumpsLeft = Jumps;
+        }
     }
 
-    // Update is called once per frame
-    void FixedUpdate ()
+    private void FixedUpdate ()
     {
+        // Check if player is on ground
+        IsOnGround = Physics2D.OverlapCircle(GroundCheck.position, CheckRadius, WhatIsGround);
+        
+        // Moving the player on the x axies
         PlayerRB.velocity = new Vector2(PlayerInputScript.MoveDirection * speed * Time.fixedDeltaTime, PlayerRB.velocity.y);
+
+        Jumping();
 	}
+
+
+    private void Jumping()
+    {
+        if (PlayerInputScript.HasPressedJump == true && JumpsLeft > 0)
+        {
+            PlayerRB.velocity = new Vector2(PlayerRB.velocity.x, JumpForce * Time.fixedDeltaTime);
+            PlayerInputScript.HasPressedJump = false;
+            JumpsLeft--;
+        }
+
+        if (PlayerInputScript.HasPressedJump == true && IsOnGround)
+        {
+            PlayerRB.velocity = new Vector2(PlayerRB.velocity.x, JumpForce * Time.fixedDeltaTime);
+            PlayerInputScript.HasPressedJump = false;
+        }
+        else
+        {
+            PlayerInputScript.HasPressedJump = false;
+        }
+
+    }
+
+    private void CheckFacingDirection()
+    {
+        if (PlayerInputScript.MoveDirection == -1 && IsFacingRight)
+        {
+            FlipPlayer();
+        }else
+        if (PlayerInputScript.MoveDirection == 1 && !IsFacingRight)
+        {
+            FlipPlayer();
+        }
+    }
+
+    private void FlipPlayer()
+    {
+        IsFacingRight = !IsFacingRight;
+
+        transform.Rotate(0f, 180f, 0f);
+    }
 }
