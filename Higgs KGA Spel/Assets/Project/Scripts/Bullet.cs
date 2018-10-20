dbb96261434e897c754526b0009b3a6713c259d2
AlexEnchanter <xalexo112@gmail.com>
2018-10-20 22:52:44 +0200

    Fixed Shooting

---
+++ Higgs KGA Spel/Assets/Project/Scripts/Bullet.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+
+    [SerializeField]private float DestroyTime = 10f;
+    private float Destroy;
+    
+
+
+    public float BulletSpeed;
+    private Rigidbody2D rb;
+	// Use this for initialization
+	void Start ()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.velocity = transform.right * BulletSpeed;
+        Destroy = DestroyTime + Time.time;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Destroy < Time.time) 
+        {
+        Destroy(gameObject);
+        }
+	}
+}
--- Higgs KGA Spel/Assets/Project/Scripts/PlayerActions.cs
+++ Higgs KGA Spel/Assets/Project/Scripts/PlayerActions.cs
@@ -4,45 +4,25 @@ using UnityEngine;
 
 public class PlayerActions : MonoBehaviour
 {
-    //To store PlayerInput script and player in a local variable
-    private GameObject Player;
-    private PlayerMovement PlayerMovementScript;
 
-    //Floats
-    [SerializeField] private float bulletspeed;
-
-    public Rigidbody2D ShotRB;
-    public Transform Shootingpoint;
+    [SerializeField]private GameObject Bulletprefab;
+    [SerializeField]private Transform Shootingpoint;
 
 	// Use this for initialization
-	void Start () {
-        Shootingpoint.GetComponent<Transform>();
-
-        ShotRB.GetComponent<Rigidbody2D>();
-
-        Player = GameObject.FindGameObjectWithTag("Player");
-
-        PlayerMovementScript = Player.GetComponent<PlayerMovement>();
+	void Start ()
+    {
+          
     }
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
 		
 	}
 
     public void shoot()
     {
-        Rigidbody2D clone;
-        clone = Instantiate(ShotRB, Shootingpoint.position, Quaternion.identity) as Rigidbody2D;
-
-        if (PlayerMovementScript.IsFacingRight)
-        {
-            clone.velocity = new Vector2(bulletspeed, 0);
-        }
-
-        if (PlayerMovementScript.IsFacingRight)
-        {
-            clone.velocity = new Vector2(-bulletspeed, 0);
-        }
+        Debug.Log("Shoot");
+        Instantiate(Bulletprefab, Shootingpoint.position, Shootingpoint.rotation);
     }
 }
